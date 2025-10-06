b64a8c3bfe69c2446812f5833708d5246014e5c4
Errayse <31817323+Errayse@users.noreply.github.com>
2025-10-06 16:46:22 +0300

    Rename planner time formatter

--- src/pages/Groups.jsx
+++ src/pages/Groups.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 export default function Groups(){
   return <div className="max-w-6xl mx-auto p-4">
-    <div className="glass p-6">Группы (MVP-заглушка)</div>
+    <div className="glass p-6">Зоны (MVP-заглушка)</div>
   </div>
 }
